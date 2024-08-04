b5db21410ba3b46846ccc7e51a1189c8783f4229
Fu Bin <fubin1991@outlook.com>
2024-08-04 22:48:07 +0800

    Update infer_MSDFont.sh

--- infer_MSDFont.sh
+++ infer_MSDFont.sh
@@ -5,6 +5,6 @@ CUDA_VISIBLE_DEVICES=3 python scripts/MSDFont_eval.py \
 --path_ttf FontData/chn/ttfs/infer_unseen_font \
 --source_path FontData/chn/source.ttf \
 --path_config_rec configs/MSDFont/MSDFont_Eval_rec_model_predx0_miniUnet.yaml \
---path_rec_model logs/rec_stage2_epoch=79-step=799999.ckpt \
+--path_rec_model logs/rec_stage2_epoch=79.ckpt \
 --path_config_trans configs/MSDFont/MSDFont_Eval_trans_model_predx0_miniUnet.yaml \
---path_trans_model logs/trans_stage_epoch=79-step=799999.ckpt
+--path_trans_model logs/trans_stage_epoch=79.ckpt
